a59af3e75e87e08cc91df107a6b9d4e4168240f6
maxeisen <17mje@queensu.ca>
2020-04-01 15:31:16 -0400

    functionality for 2018 animal counter

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<link href="index.css" rel="stylesheet">
+</head>
+<body>
+
+<h1>Hello, friend.</h1>
+<p>Hi there. Welcome to Animal Rescue, bitch!</p>
+
+<table>
+<tr><th>Number of Animals</th></tr>
+
+<?php
+
+$hostname="localhost";
+$db="SaveTheAnimalsP1Final"; #Change to your database name
+$username="root";
+$password="";
+
+$dbh = new PDO("mysql:host=$hostname;dbname=$db", $username, $password); 
+
+$rows = $dbh->query("SELECT count(animalID) FROM animalTransport WHERE destination IN (SELECT phoneNumber FROM rescueOrganization) AND year(transportDate) = 2018");
+
+echo "<h2>2018 Animals</h2>";
+
+foreach ($rows as $row) {
+	echo "<tr><td>".$row[0]."</td></tr>";
+}
+$dbh = null
+
+?>
+
+</table>
+
+</body>
+</html> 
